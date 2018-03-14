efb1828ed5f07ce0a8146516efd6578fa2ea09c5
Alex Miller <asmiller1989@gmail.com>
2018-03-14 16:23:25 -0500

    Adds babel, linter, and first basic tests

---
+++ src/pairings.js
@@ -0,0 +1,22 @@
+// Add, update, delete, or get pairings with devices
+const internalCrypto = require('./internalCrypto.js');
+const enums = require('./enums.js');
+
+// Add a pairing by signing the secret shown on the device (and typed into the app)
+exports.add = function(opts, privKey, reqFunc, cb) {
+  const { appSecret, deviceSecret, name, device } = opts;
+  const fullSecret = `${deviceSecret}${appSecret}`;
+  internalCrypto.ecsign(fullSecret, privKey, (err, sig) => {
+    if (err) { cb(err); }
+    else {
+      const payload = [name, sig];
+      const data = enums.formatArr(payload);
+      reqFunc('addPairing', data, device, cb);
+    }
+  })
+}
+
+// Remove a pairing
+exports.del = function(device, reqFunc, cb) {
+  reqFunc('delPairing', {}, device, cb);
+}
