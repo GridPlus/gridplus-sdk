e98d6c9d6a5eb00dc8c506f28a190da62d145c32
Alex Miller <asmiller1989@gmail.com>
2018-06-07 13:30:27 -0500

    Adds Ethereum node connection handler

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
-exports.commsServer = { host: 'localhost', port: 3000 }
+const leftPad = require('left-pad');
+
+exports.defaultWeb3Provider = 'http://localhost:7545';
+
+exports.erc20 = {
+  // balanceOf(address tokenOwner)
+  balanceOf: function(addr) { return `0x70a08231${leftPad(addr, '0', 64)}` },
+  decimals: function() { return `0x313ce567` },
+};
--- test/basic.js
+++ test/basic.js
@@ -4,6 +4,10 @@ const crypto = require('crypto');
 const GridPlusSDK = require('../src/index.js').default;
 let sdk;
 let privKey;
+let addr;
+
+// Handle all promise rejections
+process.on('unhandledRejection', e => { throw e; });
 
 describe('Basic tests', () => {
   it('Should instantiate an SDK object', (done) => {
@@ -17,12 +21,36 @@ describe('Basic tests', () => {
     }
   });
 
-  /*it('Should get the public header key', (done) => {
-    const key = sdk.getHeaderKey();
-    assert(typeof key === 'string');
-    assert(key.length === 130);
+  it('Should connect to an ETH node', (done) => {
+    const e = sdk.connectToEth();
+    assert(e === null);
     done();
-  });*/
+  });
 
+  it('Should get a zero ETH balance for a random address', (done) => {
+    addr = crypto.randomBytes(20).toString('hex');
+    sdk.getEthBalance(addr)
+    .then((balance) => {
+      assert(typeof balance === 'number');
+      assert(balance === 0);
+      done();
+    })
+    .catch((err) => {
+      assert(err === null, err);
+      done();
+    });
+  });
+
+  it('Should transfer ETH to the random address');
+
+  it('Should find a non-zero ETH balance for the random address');
+
+  it('Should deploy an ERC20 token');
+
+  it('Should find a zero token balance for the address');
+
+  it('Should transfer some ERC20 tokens to the address');
+
+  it('Should find a non-zero token balance for the address');
 
 })
