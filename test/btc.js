954eb92bebb88b1bd909acc1469064013daf2c30
Alex Miller <asmiller1989@gmail.com>
2018-06-15 16:40:53 -0500

    Adds a few more sample tests indicating a bitcoin transfer

--- test/btc.js
+++ test/btc.js
@@ -1,6 +1,13 @@
 // Basic tests for atomic SDK functionality
 const assert = require('assert');
+const bitcoin = require('bitcoinjs-lib');
+const config = require('../src/config.js');
 const GridPlusSDK = require('../src/index.js').default;
+const wif = require('wif');
+let startBal, startUtxos, testAddr, testKeyPair;
+
+// Handle all promise rejections
+process.on('unhandledRejection', e => { throw e; });
 
 describe('Bitcoin', () => {
   it('Should instantiate an SDK object', (done) => {
@@ -24,16 +31,43 @@ describe('Bitcoin', () => {
       done();
     })
   });
-/*
-  it('Should check the balance of the testnet address', (done) => {
-    sdk.getBalance('BTC', 'dsgadsg')
-    .then((balance) => {
+
+  // This address needs to have some BTC in it
+  it('Should check the balance of the config testnet address', (done) => {
+    sdk.getBalance('BTC', config.testing.btcHolder.address)
+    .then((d) => {
+      assert(d.balance > 0, 'Address in config.js must have >0 BTC in order to run the tests.')
+      assert(d.utxos.length > 0, 'Found zero UTXOs, but we need to spend at least one')
+      startUtxos = d.utxos;
+      startBal = d.balance;
       done();
     })
     .catch((err) => {
       assert(err === null, err);
       done();
     })
-  })
-  */
+  });
+
+  it('Should create a new testnet address', (done) => {
+    testKeyPair = bitcoin.ECPair.makeRandom({ network: bitcoin.networks.testnet });
+    testAddr = testKeyPair.getAddress();
+    done();
+  });
+
+  it('Should make a transaction spending 1 sat', (done) => {
+    try {
+      const txb = new bitcoin.TransactionBuilder();
+      const keyPair = bitcoin.ECPair.fromWIF(config.testing.btcHolder.wif, bitcoin.networks.testnet);
+      txb.addInput(startUtxos[0].hash, startUtxos[0].input);
+      txb.addOutput(testAddr, 1);
+      txb.addOutput(config.testing.btcHolder.address, startUtxos[0].value - 1);
+      txb.sign(0, keyPair)
+      console.log(txb.build().toHex())
+      done();
+    } catch (err) {
+      assert(err === null, err);
+      done();
+    }
+  });
+  
 })
