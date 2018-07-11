7e43fd517984368aea2a096bf5b577a64e2e70f8
Matt Walters <matt@gridplus.io>
2018-07-11 13:44:41 -0500

    fixing to use custom dockerfile for bcoin in order to ensure exact version

--- test/btc.js
+++ test/btc.js
@@ -19,7 +19,7 @@ const client = new NodeClient({
 let receiving = [];
 
 // Mine enough blocks so that the holder can spend the earliest
-// coinbse transaction 
+// coinbse transaction
 function mineIfNeeded(oldestUtxoHeight, done) {
   client.execute('getblockcount')
   .then((b) => {
