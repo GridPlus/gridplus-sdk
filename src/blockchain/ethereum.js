b1f0ca56a11875fa26236cf44690cc03e1ef4f3c
Alex Miller <asmiller1989@gmail.com>
2018-07-13 10:18:25 -0500

    Moves getBalance and getTransactionHistory in ETH to callbacks (and combines them)

--- src/blockchain/ethereum.js
+++ src/blockchain/ethereum.js
@@ -25,8 +25,12 @@ exports.getProvider = function() { return provider; }
 // @param [addr]      {string}  - The account we are querying
 // @param [ERC20Addr] {string}  - Address of the ERC20 token we are asking about
 // @returns           {Promise} - Contains the balance in full units (i.e. with decimals divided in)
-exports.getBalance = function(addr, ERC20Addr=null) {
+exports.getBalance = function(addr, ERC20Addr=null, cb) {
   return new Promise((resolve, reject) => {
+    let data = {
+      balance: 0,
+      transfers: {}
+    };
     if (ERC20Addr !== null) {
       if (erc20Decimals[ERC20Addr] === undefined) {
         // Save the contract as an object
@@ -36,27 +40,62 @@ exports.getBalance = function(addr, ERC20Addr=null) {
           // Get the balance
           return provider.call({ to: ERC20Addr, data: config.erc20.balanceOf(addr) })
         })
-        .then((balance) => { return resolve(parseInt(balance) / 10 ** erc20Decimals[ERC20Addr]); })
-        .catch((err) => { return reject(err); });
+        .then((balance) => { 
+          data.balance = parseInt(balance) / 10 ** erc20Decimals[ERC20Addr];
+          return getTransfers(addr, ERC20Addr)
+        })
+        .then((transfers) => {
+          data.transfers = transfers;
+          cb(null, data);
+         })
+        .catch((err) => { cb(err); });
       } else {
         // If the decimals are cached, we can just query the balance
         provider.call({ to: ERC20Addr, data: config.erc20.balanceOf(addr) })
-        .then((balance) => { return resolve(parseInt(balance) / 10 ** erc20Decimals[ERC20Addr]); })
-        .catch((err) => { return reject(err); });
+        .then((balance) => { 
+          data.balance = parseInt(balance) / 10 ** erc20Decimals[ERC20Addr];
+          return getTransfers(addr, ERC20Addr);
+        })
+        .then((transfers) => {
+          data.transfers = transfers;
+          cb(null, data);
+        })
+        .catch((err) => { cb(err); });
       }
     } else {
       // Otherwise query for the ETH balance
       provider.getBalance(addr)
-      .then((balance) => { return resolve(parseInt(balance) / 10 ** 18); })
-      .catch((err) => { return reject(err); })
+      .then((balance) => { 
+        data.balance = parseInt(balance) / 10 ** 18;
+        return getTransfers(addr, ERC20Addr)
+      })
+      .then((transfers) => {
+        data.transfers = transfers;
+        cb(null, data);
+      })
+      .catch((err) => { cb(err); })
     }
   })
 }
 
+function getTransfers(addr, ERC20Addr=null) {
+  return new Promise((resolve, reject) => {
+    if (ERC20Addr === null) {
+      // TODO: Need to figure out how to pull transfers for ETH
+      return resolve({})
+    } else {
+      getERC20TransferHistory(addr, ERC20Addr)
+      .then((transfers) =>  { return resolve(transfers); })
+      .catch((err) => { return reject(err); })
+    }
+  });
+}
+
+
 // Get a history of ERC20 transfers to and from an account
 // @param [addr]         {string}  - The account we are looking up
 // @param [contractAddr] {string}  - Address of the deployed ERC20 contract
-exports.getERC20TransferHistory = function(user, contractAddr) {
+function getERC20TransferHistory(user, contractAddr) {
   return new Promise((resolve, reject) => {
     let events = {}
     // Get transfer "out" events
@@ -85,38 +124,36 @@ exports.getNonce = function(user) {
 
 // Build an Ethereum transaction object
 // @returns {array}  - array of form [ nonce, gasPrice, gas, to, value, data ]
-exports.buildTx = function(from, to, value, opts={}) {
-  return new Promise((resolve, reject) => {
-    if (typeof from !== 'string') {
-      return reject('Please specify a single address to transfer from');
-    } else {
-      exports.getNonce(from)
-      .then((nonce) => {
-        let tx = [ nonce, null, null, to, null, null ];
-        // Fill in `value` and `data` if this is an ERC20 transfer
-        if (opts.ERC20Token !== undefined) {
-          tx[5] = config.erc20.transfer(to, value);
-          tx[4] = 0;
-          tx[3] = opts.ERC20Token;
-          tx[2] = 100000; // gas=100,000 to be safe
-        } else {
-          tx[5] = '';
-          tx[4] = value;
-          tx[2] = 22000; // gas=22000 for ETH transfers
-        }
-        // Check for a specified gas price (should be in decimal)
-        if (opts.gasPrice !== undefined) {
-          tx[1] = opts.gasPrice;
-        } else {
-          tx[1] = config.defaults.gasPrice;
-        }
-        return resolve(tx);
-      })
-      .catch((err) => {
-        return reject(err);
-      });
-    }
-  });
+exports.buildTx = function(from, to, value, opts={}, cb) {
+  if (typeof from !== 'string') {
+    cb('Please specify a single address to transfer from');
+  } else {
+    exports.getNonce(from)
+    .then((nonce) => {
+      let tx = [ nonce, null, null, to, null, null ];
+      // Fill in `value` and `data` if this is an ERC20 transfer
+      if (opts.ERC20Token !== undefined) {
+        tx[5] = config.erc20.transfer(to, value);
+        tx[4] = 0;
+        tx[3] = opts.ERC20Token;
+        tx[2] = 100000; // gas=100,000 to be safe
+      } else {
+        tx[5] = '';
+        tx[4] = value;
+        tx[2] = 22000; // gas=22000 for ETH transfers
+      }
+      // Check for a specified gas price (should be in decimal)
+      if (opts.gasPrice !== undefined) {
+        tx[1] = opts.gasPrice;
+      } else {
+        tx[1] = config.defaults.gasPrice;
+      }
+      cb(null, tx);
+    })
+    .catch((err) => {
+      cb(err);
+    });
+  } 
 }
 
 exports.broadcast = function(system, tx) {
