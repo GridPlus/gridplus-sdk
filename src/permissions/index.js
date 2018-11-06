d22c692c45f7462a2fb78d16e120f982e4eb6f6c
Alex Miller <asmiller1989@gmail.com>
2018-11-06 12:47:21 -0600

    Adds permissions logic

---
+++ src/permissions/index.js
@@ -0,0 +1,4 @@
+import { buildPermissionRequest, buildAutomatedSignatureRequest } from './permissions';
+
+exports.buildPermissionRequest = buildPermissionRequest;
+exports.buildAutomatedSignatureRequest = buildAutomatedSignatureRequest;
