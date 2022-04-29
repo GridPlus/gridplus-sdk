2d6874995abbdaf654d38bb124bcfc76ce5c1697
Doug Lance <doug.lance@gmail.com>
2022-04-29 13:27:17 -0400

    Adds export of utils
    Adds generatePrivateKey and updates client to use
    
    Combines utils to a single export point called Utils
    
    Moves getV to `src/util.ts`
    
    Limits util exports to getV and genPrivateKey

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,11 @@
 // Static utility functions
+import { Capability } from '@ethereumjs/tx';
 import aes from 'aes-js';
-import BN from 'bignumber.js';
+import { BN } from 'bn.js';
 import crc32 from 'crc-32';
 import elliptic from 'elliptic';
+import { sha256 } from 'hash.js/lib/hash/sha';
+import { ecdsaRecover } from 'secp256k1';
 import {
   AES_IV,
   BIP_CONSTANTS,
@@ -11,6 +14,7 @@ import {
   responseMsgs,
   VERSION_BYTE
 } from './constants';
+import { SignData } from './types/client';
 const { COINS, PURPOSES } = BIP_CONSTANTS;
 const EC = elliptic.ec;
 let ec;
@@ -289,4 +293,82 @@ export const randomBytes = function(n) {
     buf[i] = Math.round(Math.random() * 255);
   }
   return buf;
+}
+
+/**
+ * Generates a private key for use in maintaining connection to device.
+ * @param {Buffer} deviceId - The device ID of the device you want to generate a token for.
+ * @param {Buffer} password - The password entered when connecting to the device.
+ * @param {Buffer} appName - The name of the application.
+ * @returns a private key as a Buffer
+ */
+export const generatePrivateKey = (
+  deviceId: Buffer,
+  password: Buffer,
+  appName: Buffer
+): Buffer => {
+  const preImage = Buffer.concat([
+    deviceId,
+    password,
+    appName,
+  ]);
+
+  return Buffer.from(sha256().update(preImage).digest('hex'), 'hex');
+}
+
+/**
+ * Generic signing does not return a `v` value like legacy ETH signing requests did.
+ * Get the `v` component of the signature as well as an `initV`
+ * parameter, which is what you need to use to re-create an @ethereumjs/tx
+ * object. There is a lot of tech debt in @ethereumjs/tx which also
+ * inherits the tech debt of ethereumjs-util.
+ * 1.  The legacy `Transaction` type can call `_processSignature` with the regular
+ *     `v` value.
+ * 2.  Newer transaction types such as `FeeMarketEIP1559Transaction` will subtract
+ *     27 from the `v` that gets passed in, so we need to add `27` to create `initV`
+ * @param tx - An @ethereumjs/tx Transaction object
+ * @param resp - response from Lattice. Can be either legacy or generic signing variety
+ */
+export const getV = function (tx, resp) {
+  const hash = tx.getMessageToSign(true);
+  const rs = new Uint8Array(Buffer.concat([resp.sig.r, resp.sig.s]));
+  const pubkey = new Uint8Array(resp.pubkey);
+  const recovery0 = ecdsaRecover(rs, 0, hash, false);
+  const recovery1 = ecdsaRecover(rs, 1, hash, false);
+  const pubkeyStr = Buffer.from(pubkey).toString('hex');
+  const recovery0Str = Buffer.from(recovery0).toString('hex');
+  const recovery1Str = Buffer.from(recovery1).toString('hex');
+  let recovery;
+  if (pubkeyStr === recovery0Str) {
+    recovery = 0;
+  } else if (pubkeyStr === recovery1Str) {
+    recovery = 1;
+  } else {
+    return null;
+  }
+  // Newer transaction types just use the [0, 1] value
+  if (tx._type) {
+    return new BN(recovery);
+  }
+  // Legacy transactions should check for EIP155 support.
+  // In practice, virtually every transaction should have EIP155
+  // support since that hardfork happened in 2016...
+  // Various methods for fetching a chainID from different @ethereumjs/tx objects
+  let chainId = null;
+  if (tx.common && typeof tx.common.chainIdBN === 'function') {
+    chainId = tx.common.chainIdBN();
+  } else if (tx.chainId) {
+    chainId = new BN(tx.chainId);
+  }
+  if (!chainId || !tx.supports(Capability.EIP155ReplayProtection)) {
+    return new BN(recovery).addn(27);
+  }
+  // EIP155 replay protection is included in the `v` param
+  // and uses the chainId value.
+  return chainId.muln(2).addn(35).addn(recovery);
+};
+
+export const EXTERNAL = {
+  getV,
+  generatePrivateKey
 }
