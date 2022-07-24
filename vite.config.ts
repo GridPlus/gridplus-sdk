6382fa87a829d69e7713afa7cd82325be4004ea8
Doug Lance <doug.lance@gmail.com>
2022-07-24 09:25:12 -0400

    Fixes the shipping of types for production releases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig(({ mode }) => {
   // Loads .env file into process.env based on mode (NODE_ENV)
@@ -19,6 +20,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    plugins: [dts()],
     test: {
       coverage: {
         reporter: ['lcov'],
