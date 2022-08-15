077e01bc232b16e9f7715ae5ced7d204e52dcbe0
Doug Lance <doug.lance@gmail.com>
2022-08-15 19:13:46 -0400

    Adds mock data for fetchCalldataDecoder tests

--- src/__test__/integration/__mocks__/4byte.ts
+++ src/__test__/integration/__mocks__/4byte.ts
@@ -29,7 +29,7 @@ export const fourbyteResponse0xa9059cbb = {
       text_signature: 'func_2093253501(bytes)',
     },
   ],
-}
+};
 
 export const fourbyteResponse0x38ed1739 = {
   results: [
@@ -42,4 +42,54 @@ export const fourbyteResponse0x38ed1739 = {
         'swapExactTokensForTokens(uint256,uint256,address[],address,uint256)',
     },
   ],
-}
+};
+
+export const fourbyteResponseac9650d8 = {
+  results: [
+    {
+      id: 134730,
+      created_at: '2018-10-13T08:40:29.456228Z',
+      text_signature: 'multicall(bytes[])',
+      hex_signature: '0xac9650d8',
+      bytes_signature: '¬\x96PØ',
+    },
+  ],
+};
+
+export const fourbyteResponse0c49ccbe = {
+  results: [
+    {
+      id: 186682,
+      created_at: '2021-05-09T03:48:17.627742Z',
+      text_signature:
+        'decreaseLiquidity((uint256,uint128,uint256,uint256,uint256))',
+      hex_signature: '0x0c49ccbe',
+      bytes_signature: '\\fIÌ¾',
+    },
+  ],
+};
+
+export const fourbyteResponsefc6f7865 = {
+  results: [
+    {
+      id: 186681,
+      created_at: '2021-05-09T03:48:17.621683Z',
+      text_signature: 'collect((uint256,address,uint128,uint128))',
+      hex_signature: '0xfc6f7865',
+      bytes_signature: 'üoxe',
+    },
+  ],
+};
+
+export const fourbyteResponse0x6a761202 = {
+  results: [
+    {
+      id: 169422,
+      created_at: '2020-01-28T10:40:07.614936Z',
+      text_signature:
+        'execTransaction(address,uint256,bytes,uint8,uint256,uint256,uint256,address,address,bytes)',
+      hex_signature: '0x6a761202',
+      bytes_signature: 'jv\\u0012\\u0002',
+    },
+  ],
+};
