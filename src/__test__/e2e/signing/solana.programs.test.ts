e9da218bcc9001056c131c7848f54f6e152eca08
Doug Lance <doug.lance@gmail.com>
2022-10-17 12:39:00 -0400

    Adds Solana test contracts and adds solana to launch.json

---
+++ src/__test__/e2e/signing/solana.programs.test.ts
@@ -0,0 +1,35 @@
+import { Constants } from '../../..';
+import { initializeClient } from '../../utils/initializeClient';
+import { dexlabProgram, raydiumProgram } from './__mocks__/programs';
+
+describe('Solana Programs', () => {
+  const client = initializeClient();
+
+  it('should sign Dexlab program', async () => {
+    const payload = dexlabProgram;
+    const signedMessage = await client.sign({
+      data: {
+        signerPath: [0x80000000 + 44, 0x80000000 + 501, 0x80000000],
+        curveType: Constants.SIGNING.CURVES.ED25519,
+        hashType: Constants.SIGNING.HASHES.NONE,
+        encodingType: Constants.SIGNING.ENCODINGS.SOLANA,
+        payload,
+      },
+    });
+    expect(signedMessage).toBeTruthy();
+  });
+
+  it('should sign Raydium program', async () => {
+    const payload = raydiumProgram;
+    const signedMessage = await client.sign({
+      data: {
+        signerPath: [0x80000000 + 44, 0x80000000 + 501, 0x80000000],
+        curveType: Constants.SIGNING.CURVES.ED25519,
+        hashType: Constants.SIGNING.HASHES.NONE,
+        encodingType: Constants.SIGNING.ENCODINGS.SOLANA,
+        payload,
+      },
+    });
+    expect(signedMessage).toBeTruthy();
+  });
+});
