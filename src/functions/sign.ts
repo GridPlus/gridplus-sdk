4017c24d3088d18a88f878d2f8b5c378a8ee5867
Alex Miller <asmiller1989@gmail.com>
2022-12-20 12:58:20 -0600

    Prevents Bitcoin requests that use unsupported P2WSH address type

--- src/functions/sign.ts
+++ src/functions/sign.ts
@@ -35,7 +35,7 @@ export async function sign ({
     wallet: client.getActiveWallet(),
     sharedSecret: client.sharedSecret,
   });
-
+  
   const { request, isGeneric } = buildTransaction({
     data,
     currency,
