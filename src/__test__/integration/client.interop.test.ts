655fdd9de20eaac4ff3cebbd0cba26888793ba07
netbonus <151201453+netbonus@users.noreply.github.com>
2024-10-16 10:34:42 -0400

    skip interop right now

--- src/__test__/integration/client.interop.test.ts
+++ src/__test__/integration/client.interop.test.ts
@@ -6,7 +6,7 @@ import { getStoredClient, setStoredClient } from '../utils/setup';
 /**
  * This test is used to test the interoperability between the Class-based API and the Functional API.
  */
-describe('client interop', () => {
+describe.skip('client interop', () => {
   it('should setup the Client, then use that client data to', async () => {
     const client = setupTestClient();
     const isPaired = await client.connect(getDeviceId());
