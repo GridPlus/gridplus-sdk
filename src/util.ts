098a6a2770a7370ccfcef4561bf701f3a3bbd78e
netbonus <151201453+netbonus@users.noreply.github.com>
2024-11-07 16:56:27 -0500

    remove response error

--- src/util.ts
+++ src/util.ts
@@ -26,7 +26,6 @@ import {
   isValidBlockExplorerResponse,
 } from './shared/validators';
 import { FirmwareConstants } from './types';
-import { LatticeResponseError } from './shared/errors';
 
 const { COINS, PURPOSES } = BIP_CONSTANTS;
 let ec: EC | undefined;
