a7be7d6eeef1b16d4400d1628e64cebaa0b3191a
baha <yilmazbahadir@gmail.com>
2026-02-10 17:08:08 +0300

    feat(sdk): add lattice signers bridge for asset modules

---
+++ packages/sdk/src/assets/index.ts
@@ -0,0 +1,8 @@
+export {
+  createLatticeBtcSigner,
+  createLatticeCosmosSigner,
+  createLatticeEvmSigner,
+  createLatticeSolanaSigner,
+} from './lattice';
+
+export type { LatticeSignerOptions } from './lattice';
--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,4 +3,5 @@ export { Client } from './client';
 export { EXTERNAL as Constants } from './constants';
 export { EXTERNAL as Utils } from './util';
 export * from './api';
+export * from './assets';
 export * as btc from './btc';
